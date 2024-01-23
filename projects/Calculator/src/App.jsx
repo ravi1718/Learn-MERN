79d86a2a18d42d0bc6dae93ef515887447983308
ravi1718 <neeliravitej@gmail.com>
2024-01-23 19:21:58 +0530

    commit

--- projects/Calculator/src/App.jsx
+++ projects/Calculator/src/App.jsx
@@ -1,16 +1,12 @@
 import Display from "./components/Display";
+import ButtonsContainer from "./components/ButtonsContainer";
 import styles from "./App.module.css";
 
 function App() {
   return (
     <div className={styles.calculator}>
       <Display></Display>
-      <div className={styles.buttonsContainer}>
-        <button className={styles.buttons}>C</button>
-        <button className={styles.buttons}>1</button>
-        <button className={styles.buttons}>2</button>
-        <button className={styles.buttons}>+</button>
-      </div>
+     <ButtonsContainer></ButtonsContainer>
     </div>
   );
 }
